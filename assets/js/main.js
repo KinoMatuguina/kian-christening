71986ddc89dda58324251c7ef85576cbf3b72fa3
kinomatuguina <kino.bacani.matuguina@gmail.com>
2016-09-15 16:32:18 +0800

    make burger nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,52 @@
-$('.single-item').slick({
-	  slide: 'div',
-	  dots: false,
-	  infinite: true,
-	  autoplay: true,
-	  slidesToShow: 1,
-	  prevArrow: false,
-	  nextArrow: false
-});
-	
+
+(function($) {
+    'use strict';
+
+
+    $.fn.christening = function(options) {
+    	var _this = this,
+            defaults = {
+                //Basic variables
+                nav: $('.burger-navigation'),
+                nav_collapse: $('.head-collapse'),
+                THIS: $(this)
+            },
+            x = this.config = $.extend(defaults, options),
+
+        	main = {
+
+	        	slick: function() {
+
+	        		$('.single-item').slick({
+						  slide: 'div',
+						  dots: false,
+						  infinite: true,
+						  autoplay: true,
+						  slidesToShow: 1,
+						  prevArrow: false,
+						  nextArrow: false
+					});
+
+	        	},
+
+	        	toggle: function() {
+
+	        		x.nav.on('click', function(){
+	        			x.nav.toggleClass('active');
+	        				if (x.nav.is('.active')) {
+	        					x.nav_collapse.slideDown('fast');
+	        				} else {
+	        					x.nav_collapse.slideUp('fast');
+	        				}
+	        		});
+	        	}
+
+        }
+
+        
+        	main.slick();
+        	main.toggle();
+
+    }
+
+}(jQuery));
